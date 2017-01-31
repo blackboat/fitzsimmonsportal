73a20ca5933a478ee29b2c6e63ecf76315f51b72
blackboat <zygroup1@gmail.com>
2017-01-31 20:54:53 -0200

    session approve/reject

--- wp-content/themes/dazzling/woocommerce/emails/admin-new-order.php
+++ wp-content/themes/dazzling/woocommerce/emails/admin-new-order.php
@@ -53,6 +53,58 @@
   */
  do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
 
+if ($order->get_total() >= get_approval_threshold()) { ?>
+ <p style="margin-top:20px !important;">
+    <a class="btn btn-success"
+       style="display: inline-block;
+            font-weight: 400;
+            line-height: 1.25;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            border: 1px solid transparent;
+            padding: .5rem 1rem;
+            font-size: 1rem;
+            border-radius: .25rem;
+            -webkit-transition: all .2s ease-in-out;
+            -o-transition: all .2s ease-in-out;
+            transition: all .2s ease-in-out;
+            background-color: #5cb85c;
+            border-color: #5cb85c;
+            display: inline-block;
+            text-decoration: none !important;
+            margin-right: 50px;"
+       href="<?php echo approve_link; ?>">Approve</a>
+    <a class="btn btn-danger"
+       style="display: inline-block;
+            font-weight: 400;
+            line-height: 1.25;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+            border: 1px solid transparent;
+            padding: .5rem 1rem;
+            font-size: 1rem;
+            border-radius: .25rem;
+            -webkit-transition: all .2s ease-in-out;
+            -o-transition: all .2s ease-in-out;
+            transition: all .2s ease-in-out;
+            background-color: #d9534f;
+            border-color: #d9534f;
+            color: #fff;
+            text-decoration: none !important;"
+       href="<?php echo reject_link; ?>">Reject</a>
+</p>
+<?php } ?>
+
  /**
   * @hooked WC_Emails::email_footer() Output the email footer
   */
